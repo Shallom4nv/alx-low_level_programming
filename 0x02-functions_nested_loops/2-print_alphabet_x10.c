edd5907795f0bea454ca47ad831ce5d0e97a9294
oluwaseun.james@gmail.com <jamesoluwaseun@gmail.com>
2022-10-19 11:31:50 -0700

    task 22

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
  * Return: 0, if fail
  */
-
 void print_alphabet_x10(void)
-
 {
 	char i, k;
-	
+
 	for (k = 0; k < 10; k++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
