e3aaf74a2046690381fb7293caa900ccfe9d4f3a
oluwaseun.james@gmail.com <jamesoluwaseun@gmail.com>
2022-11-01 11:50:57 -0700

    task700

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copy Memory
+ * @dest: dest memory area
+ * @src: source memory area
+ * @n: bytes from memoryrea to copy
+ * Return: memory area fill
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is work solution to task 0x07 pointer to a pointer
