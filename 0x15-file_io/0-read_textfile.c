c07543e2938c3b5c21e23fa0cf1f1f7d974459d5
oluwaseun.james@gmail.com <jamesoluwaseun@gmail.com>
2022-12-06 06:08:08 -0800

    task0x1511

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,8 @@
  * read_textfile - reads a text file and prints the lette
  * @filename: filename.
  * @letters: numbers of letters printed.
- * Return: numbers of letters printed. It fails, returns 0 */
+ * Return: numbers of letters printed. It fails, returns 0
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -16,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);\
+		return (0);
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
